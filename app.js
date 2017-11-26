2a88442dd9ac7e943fe05c47b9829787cefd6f8e
thu <mingfei.net@gmail.com>
2017-11-26 11:27:44 +0800

    Update

--- app.js
+++ app.js
@@ -7,3 +7,17 @@
  * Date: 2017/11/26 9:59
  */
 
+const express = require('express');
+const path = require('path');
+
+let app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/default.html'));
+});
+
+app.get('/sign-up', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/sign-up.html'));
+});
+
+app.listen(80);
